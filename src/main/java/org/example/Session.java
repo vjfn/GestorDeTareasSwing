3db4e9cf14dc3a878141290be8f422fa06db3f35
VíctorJesúsFernández <victor@comit.com>
2023-10-05 11:02:40 +0200

    version0.3 Guardado de Datos

---
+++ src/main/java/org/example/Session.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Session {
+
+    private static String username = null;
+
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        Session.username = username;
+    }
+
+
+
+
+
+}
--- src/main/java/ui/Principal.java
+++ src/main/java/ui/Principal.java
@@ -1,11 +1,14 @@
 package ui;
 
+import org.example.Session;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.*;
 
 public class Principal extends JFrame {
     private JPanel panel;
@@ -16,13 +19,14 @@ public class Principal extends JFrame {
     private JButton añadirButton;
     private JTextArea textArea1;
     private JLabel txtDescripcion;
+    private JLabel info;
     private DefaultTableModel datos;
 
     public Principal(){
         this.setContentPane(panel);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(500, 300);
-        setTitle("Listado tareas");
+        setTitle("Listado de tareas");
         setResizable(false);
         setLocationRelativeTo(null);
 
@@ -32,14 +36,36 @@ public class Principal extends JFrame {
         datos.addColumn("Estado");
         datos.addColumn("Descripció");
 
-//        JFrame frame = new JFrame ("Test");
-//        JTextArea textArea = new JTextArea ("Test");
-//
-//        JScrollPane scroll = new JScrollPane (textArea,
-//                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-//
-//        frame.add(scroll);
-//        frame.setVisible (true);
+        loadUserData();
+        table1.setModel(datos);
+
+        info.setText("Bienvenido "+ Session.getUsername());
+
+        //JMenuBar
+        var menuBar = new JMenuBar();
+        var menu = new JMenu("Archivo");
+
+        var menuItem0 = new JMenuItem("Guardar");
+        menu.add(menuItem0);
+        menu.addSeparator();
+
+        var menuItem1 = new JMenuItem("Logout");
+        menu.add(menuItem1);
+        menu.addSeparator();
+
+        var menuItem2 = new JMenuItem(("Salir"));
+        menu.add(menuItem2);
+        menu.addSeparator();
+
+
+        menuBar.add(menu);
+
+        setJMenuBar(menuBar);
+
+        menuItem0.addActionListener(e -> actionMenuGuardar() );
+        menuItem1.addActionListener(e -> actionMenu1() );
+        menuItem2.addActionListener(e -> actionMenu2() );
+
 
         añadirButton.addActionListener(e -> {addToTable();});
         table1.addMouseListener(new MouseAdapter() {
@@ -47,9 +73,46 @@ public class Principal extends JFrame {
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
                 readTable();
+                (new Login()).load();
             }
         });
     }
+
+    private void actionMenuGuardar() {
+        String f = Session.getUsername() + ".obj";
+        try (var oos = new ObjectOutputStream(new FileOutputStream(f))) {
+            oos.writeObject(datos);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void loadUserData() {
+        String f = Session.getUsername() + ".obj";
+        try (var ois = new ObjectInputStream(new FileInputStream("f"))) {
+            datos = (DefaultTableModel) ois.readObject();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void actionMenu2() {
+        System.exit(0);
+    }
+
+    private void actionMenu1() {
+        Session.setUsername(null);
+        this.dispose();
+        (new Login()).load();
+
+    }
+
     private void readTable(){
         var selected = table1.getSelectedRow();
         System.out.println(selected);
